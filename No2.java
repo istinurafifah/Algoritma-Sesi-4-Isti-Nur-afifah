080d604c0e346e21e29bc0414f2108ec9c1de13f
isti nurafifah <nurafifahisti07@gmail.com>
2024-10-20 22:13:42 +0700

    percabangan

---
+++ No2.java
@@ -0,0 +1,16 @@
+public class No2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) { 
+            for (int j = 1; j <= 6; j++) { 
+                
+                if (j == 1) {
+                    System.out.print(i + " "); 
+                } else {
+                    System.out.print((i + j - 1) + " "); 
+                }
+            }
+            System.out.println(); 
+        }
+    }
+}
+    
---
+++ No4.java
@@ -0,0 +1,19 @@
+public class No4 {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 3; a++) {
+            for (int b = 1; b <= 6; b++) {
+                if ((b % 2 == 1) ^ (a == 2)){
+                    System.out.print("s ");
+                } else {
+                    System.out.print("0 ");
+                  }
+                }
+                    System.out.println("");
+                        
+         }
+    }
+    
+}
+
+    
+
